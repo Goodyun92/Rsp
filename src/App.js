8e48e2f5c8c303fa737b5c106fd004a252aab477
Goodyun92 <0902hdy@gmail.com>
2023-07-26 14:29:21 +0900

    hand error

--- src/App.js
+++ src/App.js
@@ -11,9 +11,13 @@ function App() {
   // 2:sci
   // 3:pap
 
+  //같은거 연속으로 냈을때 결과판정오류남
+  //useEffect 두번째 인자를 hand가 아닌 round로
+
   const [myHand, setMyHand] = useState(0);
   const [comHand, setComHand] = useState(0);
   const [result, setResult] = useState("Ready...");
+  const [round, setRound] = useState(0);
 
   useEffect( ()=>{
     if(myHand>0){
@@ -24,11 +28,12 @@ function App() {
       else setResult("Lose...");
       console.log({result})
     }
-  }, [myHand])
+  }, [round])
 
   const clickHand = (hand) => {
     setMyHand(hand)
     setComHand(parseInt((Math.random()*(3)))+1)
+    setRound(round+1)
   }
   
   return (
